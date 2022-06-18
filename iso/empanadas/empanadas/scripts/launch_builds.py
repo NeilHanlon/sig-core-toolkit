3e85e161bdd9d1bc2ef28cdd17f533380437e485
Neil Hanlon <neil@rockylinux.org>
2022-06-18 14:52:03 -0400

    Utilize skbn to upload images to a bucket

--- iso/empanadas/empanadas/scripts/launch_builds.py
+++ iso/empanadas/empanadas/scripts/launch_builds.py
@@ -10,31 +10,27 @@ from jinja2 import Environment, FileSystemLoader
 parser = argparse.ArgumentParser(description="ISO Compose")
 
 parser.add_argument('--release', type=str, help="Major Release Version", required=True)
-parser.add_argument('--isolation', type=str, help="mock isolation mode")
-parser.add_argument('--rc', action='store_true', help="Release Candidate")
-parser.add_argument('--local-compose', action='store_true', help="Compose Directory is Here")
-parser.add_argument('--logger', type=str)
+parser.add_argument('--env', type=str, help="environment", required=True)
 results = parser.parse_args()
 rlvars = rldict[results.release]
 major = rlvars['major']
+
+EXTARCH=["s390x", "ppc64le"]
+EKSARCH=["amd64", "arm64"]
 
 def run():
     file_loader = FileSystemLoader(f"{_rootdir}/templates")
     tmplenv = Environment(loader=file_loader)
     job_template = tmplenv.get_template('kube/Job.tmpl')
+
+    arches = EKSARCH
+    if results.env == "ext" and results.env != "all":
+        arches = EXTARCH
+    elif results.env == "all":
+        arches = EKSARCH+EXTARCH
+
     out = ""
-    for arch in ["amd64", "arm64"]: 
-        out += job_template.render(
-            architecture=arch,
-            backoffLimit=4,
-            command=["build-iso", "--release", "9", "--rc", "--isolation", "simple"],
-            containerName="buildiso",
-            imageName="ghcr.io/neilhanlon/sig-core-toolkit:latest",
-            jobName=f"build-iso-{arch}",
-            namespace="empanadas",
-            restartPolicy="Never",
-        )
-    for arch in ["s390x", "ppc64le"]: 
+    for arch in arches:
         out += job_template.render(
             architecture=arch,
             backoffLimit=4,
